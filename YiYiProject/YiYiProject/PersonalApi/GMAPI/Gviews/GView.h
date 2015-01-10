66936af5469f7835c21492b823bda0469abc64f7
gaomeng <gaomengaz@163.com>
2015-01-10 22:42:25 +0800

    1
    1

---
+++ YiYiProject/YiYiProject/PersonalApi/GMAPI/Gviews/GView.h
@@ -0,0 +1,16 @@
+//
+//  GView.h
+//  YiYiProject
+//
+//  Created by gaomeng on 15/1/10.
+//  Copyright (c) 2015年 lcw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GView : UIView
+
+
+@property(nonatomic,strong)NSString *gString;
+
+@end
